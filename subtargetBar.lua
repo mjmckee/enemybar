390e72c2d3356c4964c3ad3f386c962ce19fb581
mjmckee <ssjchrono@gmail.com>
2015-08-28 21:55:33 -0700

    Ver. 0.3.2
    +Text color change correctly updates
    +Subtarget color change implemented
    -Messy code

--- subtargetBar.lua
+++ subtargetBar.lua
@@ -1,10 +1,8 @@
 --[[
 Copyright © 2015, Mike McKee
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +11,6 @@ modification, are permitted provided that the following conditions are met:
     * Neither the name of enemybar nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,66 +27,67 @@ stbg_cap_l = images.new()
 stbg_cap_r = images.new()
 stbg_body = images.new()
 stfg_body = images.new()
-txtSubTarget = texts.new()
+st_text = texts.new()
 
-init_subTargetImages = function(...)
-	stbg_cap_l:pos(settings.global.X + 399, settings.global.Y + 15)
-	stbg_cap_l:path(settings.global.bg_cap_path)
+init_subtarget_images = function(...)
+	stbg_cap_l:pos(settings.X + 399, settings.Y + 15)
+	stbg_cap_l:path(bg_cap_path)
 	stbg_cap_l:color(255, 255, 255, 255)
 	stbg_cap_l:fit(true)
-	stbg_cap_l:size(settings.global.subtargetBarWidth, settings.global.subtargetBarHeight)
+	stbg_cap_l:size(settings.subtargetBarWidth, settings.subtargetBarHeight)
 	stbg_cap_l:repeat_xy(1, 1)
 	
-	stbg_cap_r:pos(settings.global.X + settings.global.subtargetBarWidth + 400, settings.global.Y + 15)
-	stbg_cap_r:path(settings.global.bg_cap_path)
+	stbg_cap_r:pos(settings.X + settings.subtargetBarWidth + 400, settings.Y + 15)
+	stbg_cap_r:path(bg_cap_path)
 	stbg_cap_r:color(255, 255, 255, 255)
 	stbg_cap_r:fit(true)
-	stbg_cap_r:size(settings.global.subtargetBarWidth, settings.global.subtargetBarHeight)
+	stbg_cap_r:size(settings.subtargetBarWidth, settings.subtargetBarHeight)
 	stbg_cap_r:repeat_xy(1, 1)
 
-	stbg_body:pos(settings.global.X + 400, settings.global.Y + 15)
-	stbg_body:path(settings.global.stbg_body_path)
+	stbg_body:pos(settings.X + 400, settings.Y + 15)
+	stbg_body:path(stbg_body_path)
 	stbg_body:fit(true)
-	stbg_body:size(settings.global.subtargetBarWidth, settings.global.subtargetBarHeight)
+	stbg_body:size(settings.subtargetBarWidth, settings.subtargetBarHeight)
 	stbg_body:repeat_xy(1, 1)
 	
-    stfg_body:pos(settings.global.X + 400, settings.global.Y + 15)
-	stfg_body:path(settings.global.stfg_body_path)
+    stfg_body:pos(settings.X + 400, settings.Y + 15)
+	stfg_body:path(stfg_body_path)
 	stfg_body:fit(true)
-	stfg_body:size(settings.global.subtargetBarWidth, settings.global.subtargetBarHeight)
+	stfg_body:size(settings.subtargetBarWidth, settings.subtargetBarHeight)
 	stfg_body:repeat_xy(1, 1)
 	
-	txtSubTarget:pos(settings.global.X + 400, settings.global.Y + 15)
-	txtSubTarget:font(settings.global.font)
-	txtSubTarget:size(settings.global.size)
-	txtSubTarget:bold(true)
-	txtSubTarget:text('Sub Name')
+	st_text:pos(settings.X + 400, settings.Y + 15)
+	st_text:font(settings.font)
+	st_text:size(settings.textSize)
+	st_text:bold(true)
+	st_text:text('Sub Name')
 	
-	txtSubTarget:bg_visible(false)
-	txtSubTarget:color(255, 255, 255)
-	txtSubTarget:alpha(255)
+	st_text:bg_visible(false)
+	st_text:color(255, 255, 255)
+	st_text:alpha(255)
 	
-	txtSubTarget:stroke_width(settings.global.strkSize)
-	txtSubTarget:stroke_color(50, 50, 50)
-	txtSubTarget:stroke_transparency(127)
+	st_text:stroke_width(settings.strokeSize)
+	st_text:stroke_color(50, 50, 50)
+	st_text:stroke_transparency(127)
 end
 
-renderSubTargetBar = function(...)
-	if settings.global.visible == true then
+render_subtarget_bar = function(...)
+	if visible == true then
 		local subtarget = windower.ffxi.get_mob_by_target('st')
+		local player = windower.ffxi.get_player()
 		
 		if subtarget ~= nil then
 			stbg_cap_l:show()
 			stbg_cap_r:show()
 			stbg_body:show()
 			stfg_body:show()
-			txtSubTarget:show()
+			st_text:show()
 			
 			local old_width = stfg_body:width()
 			local i = subtarget.hpp / 100
 			local new_width = math.floor(198 * i)	
 			
-			if settings.global.style == 1 then
+			if settings.style == 1 then
 				if new_width ~= nil and new_width > 0 then
 					if old_width > new_width then
 						local last_update = 0
@@ -109,13 +107,27 @@ renderSubTargetBar = function(...)
 			end
 						
 			stbg_body:size(198, 12)
-			txtSubTarget:text('  ' .. subtarget.name)
+			st_text:text('  ' .. subtarget.name)
+			
+			if check_claim(subtarget.claim_id) then
+				st_text:color(255, 80, 80)
+			elseif subtarget.in_party == true and subtarget.id ~= player.id then
+				st_text:color(102, 255, 255)
+			elseif subtarget.is_npc == false then
+				st_text:color(255, 255, 255)
+			elseif subtarget.claim_id == 0 then
+				st_text:color(230, 230, 138) 
+			elseif subtarget.hpp == 0 then
+				st_text:color(155, 155, 155)
+			elseif subtarget.claim_id ~= 0 then
+				st_text:color(153, 102, 255)
+			end
 		else
 			stbg_cap_l:hide()
 			stbg_cap_r:hide()
 			stbg_body:hide()
 			stfg_body:hide()
-			txtSubTarget:hide()
+			st_text:hide()
 		end
 	end
 end
--- targetBar.lua
+++ targetBar.lua
@@ -1,10 +1,8 @@
 --[[
 Copyright © 2015, Mike McKee
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +11,6 @@ modification, are permitted provided that the following conditions are met:
     * Neither the name of enemybar nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,68 +27,70 @@ tbg_cap_l = images.new()
 tbg_cap_r = images.new()
 tbg_body = images.new()
 tfg_body = images.new()
+t_text = texts.new()
 
-txtTarget = texts.new()
 
-init_targetImages = function(...)
-	tbg_cap_l:pos(settings.global.X - 1, settings.global.Y)
-	tbg_cap_l:path(settings.global.bg_cap_path)
+
+init_target_images = function(...)
+	tbg_cap_l:pos(settings.X - 1, settings.Y)
+	tbg_cap_l:path(bg_cap_path)
 	tbg_cap_l:color(255, 255, 255, 255)
 	tbg_cap_l:fit(true)
-	tbg_cap_l:size(settings.global.targetBarWidth, settings.global.targetBarHeight)
+	tbg_cap_l:size(settings.targetBarWidth, settings.targetBarHeight)
 	tbg_cap_l:repeat_xy(1, 1)
 	
-	tbg_cap_r:pos(settings.global.X + settings.global.targetBarWidth, settings.global.Y)
-	tbg_cap_r:path(settings.global.bg_cap_path)
+	tbg_cap_r:pos(settings.X + settings.targetBarWidth, settings.Y)
+	tbg_cap_r:path(bg_cap_path)
 	tbg_cap_r:color(255, 255, 255, 255)
 	tbg_cap_r:fit(true)
-	tbg_cap_r:size(settings.global.targetBarWidth, settings.global.targetBarHeight)
+	tbg_cap_r:size(settings.targetBarWidth, settings.targetBarHeight)
 	tbg_cap_r:repeat_xy(1, 1)
 
-	tbg_body:pos(settings.global.X, settings.global.Y)
-	tbg_body:path(settings.global.tbg_body_path)
+	tbg_body:pos(settings.X, settings.Y)
+	tbg_body:path(tbg_body_path)
 	tbg_body:fit(true)
 	tbg_body:size(598, 12)
 	tbg_body:repeat_xy(1, 1)
 		
-	tfg_body:pos(settings.global.X, settings.global.Y)
-	tfg_body:path(settings.global.tfg_body_path)
+	tfg_body:pos(settings.X, settings.Y)
+	tfg_body:path(tfg_body_path)
 	tfg_body:fit(true)
-	tfg_body:size(settings.global.targetBarWidth, settings.global.targetBarHeight)
+	tfg_body:size(settings.targetBarWidth, settings.targetBarHeight)
 	tfg_body:repeat_xy(1, 1)
 	
-	txtTarget:pos(settings.global.X, settings.global.Y)
-	txtTarget:font(settings.global.font)
-	txtTarget:size(settings.global.size)
-	txtTarget:bold(true)
-	txtTarget:text('Enemy Name')
+	t_text:pos(settings.X, settings.Y)
+	t_text:font(settings.font)
+	t_text:size(settings.textSize)
+	t_text:bold(true)
+	t_text:text('Enemy Name')
 	
-	txtTarget:bg_visible(false)
-	txtTarget:color(255, 255, 255)
-	txtTarget:alpha(255)
+	t_text:bg_visible(false)
+	t_text:color(255, 255, 255)
+	t_text:alpha(255)
 	
-	txtTarget:stroke_width(settings.global.strkSize)
-	txtTarget:stroke_color(50, 50, 50)
-	txtTarget:stroke_transparency(127)
+	t_text:stroke_width(settings.strokeSize)
+	t_text:stroke_color(50, 50, 50)
+	t_text:stroke_transparency(127)
 end
 
-renderTargetBar = function (...)
-	if settings.global.visible == true then
+render_target_bar = function (...)
+	if visible == true then
 		tbg_cap_l:show()
 		tbg_cap_r:show()
 		tbg_body:show()
 		tfg_body:show()
-		txtTarget:show()
+		t_text:show()
 		
 		local target = windower.ffxi.get_mob_by_target('t')
 		local player = windower.ffxi.get_player()
+		local party = windower.ffxi.get_party()
 		
 		if target ~= nil then
 			local old_width = tfg_body:width()
 			local i = target.hpp / 100
-			local new_width = math.floor(settings.global.targetBarWidth * i)
+			local new_width = math.floor(settings.targetBarWidth * i)
 			
-			if settings.global.style == 1 then
+			if settings.style == 1 then
 				--Animated Style 'borrowed' from Morath's barfiller
 				if new_width ~= nil and new_width > 0 then
 					if old_width > new_width then
@@ -113,24 +112,21 @@ renderTargetBar = function (...)
 			end
 			
 			tbg_body:size(598, 12)
-			--Update the Text
-			txtTarget:text('  ' .. target.name .. ' - HP ' .. target.hpp .. '%')
-			if player.in_combat == true then
-				txtTarget:color(255, 80, 80)
-			else
-				if target.is_npc == false then
-					txtTarget:color(255, 255, 255)
-				else
-					if target.claim_id == 0 then
-						txtTarget:color(230, 230, 138)
-					else 
-						if target.hpp == 0 then
-							txtTarget:color(155, 155, 155)
-						else
-							txtTarget:color(153, 102, 255)
-						end
-					end
-				end
+			t_text:text('  ' .. target.name .. ' - HP ' .. target.hpp .. '%')
+			
+			--Check claim_id with player and party_id 
+			if check_claim(target.claim_id) then
+				t_text:color(255, 80, 80)
+			elseif target.in_party == true and target.id ~= player.id then
+				t_text:color(102, 255, 255)
+			elseif target.is_npc == false then
+				t_text:color(255, 255, 255)
+			elseif target.claim_id == 0 then
+				t_text:color(230, 230, 138) 
+			elseif target.hpp == 0 then
+				t_text:color(155, 155, 155)
+			elseif target.claim_id ~= 0 then
+				t_text:color(153, 102, 255)
 			end
 		end
 		
@@ -139,6 +135,6 @@ renderTargetBar = function (...)
 		tbg_cap_r:hide()
 		tbg_body:hide()
 		tfg_body:hide()
-		txtTarget:hide()
+		t_text:hide()
 	end
 end
